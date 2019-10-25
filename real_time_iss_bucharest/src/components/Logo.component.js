21d72494793b973c3477bce6ceb22a5cad6461f9
Maria <fmariamarinescu@gmail.com>
2019-10-25 11:37:59 +0300

    creates HTML logo

--- real_time_iss_bucharest/src/components/Logo.component.js
+++ real_time_iss_bucharest/src/components/Logo.component.js
@@ -4,4 +4,36 @@ class Logo extends Component {
     constructor(props) {
         super(props);
     }
+    render() {
+        return(
+        <div id="logo">
+            <div id="atom">
+                <div class="orbit">
+                    <div class="path">
+                        <div class="electron"></div>
+                    </div>
+                </div>
+                <div class="orbit">
+                    <div class="path">
+                        <div class="electron"></div>
+                    </div>
+                </div>
+                <div class="orbit">
+                    <div class="path">
+                        <div class="electron"></div>
+                    </div>
+                </div>
+                <div class="orbit">
+                    <div class="path">
+                        <div class="electron"></div>
+                    </div>
+                </div>
+                <div id="nucleus"></div>
+            </div>
+        </div>
+
+
+        )
+    }
 }
+export default Logo;
