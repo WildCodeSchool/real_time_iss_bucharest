4c15594691048d9c04873b94e43ade5d4f6818fc
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-10-31 12:32:32 +0200

    welcome section with style but without the logo

--- real_time_iss_bucharest/src/App.js
+++ real_time_iss_bucharest/src/App.js
@@ -3,7 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 import Welcome from './components/Welcome'
 
 function App() {
--- real_time_iss_bucharest/src/components/Welcome.js
+++ real_time_iss_bucharest/src/components/Welcome.js
@@ -9,21 +9,20 @@ class Welcome extends Component {
     }
     render() {
         return(
-        <div className='welcome'>
-            <p>Hi, I'm the welcome component</p>
-            <div className="container">
-                <div className="row">
+        <div className='welcome container-fluid row align-items-center p-3 mb-2 bg-light text-dark'>
+            <div className="container welcome-main">
+                <div className="row align-items-center">
                     <div className="col-5">
-                        Maria's awesome logo goes here
+                        <div className="logo">Maria's awesome logo goes here</div>
                     </div>
                     <div className="col-7">
                         <div className="row">
-                            <h1>Real-timer ISS tracker</h1>
-                            <h2>
+                            <h1 className="display-3">Real-time ISS tracker</h1>
+                            <h2 className="lead">
                             The International Space Station is a space station in low Earth orbit, at approximately 400 km from the ground.<br/>
                             At 28 800 km/h it only takes 90 minutes for the weightless laboratory to make a complete circuit of Earth.
                             </h2>
-                            <button className="callToAction col-4">Track ISS</button>
+                                <button className="welcome-call-to-action col-4 btn btn-dark animated bounce">Track ISS</button>
                         </div>
                     </div>
                 </div>
