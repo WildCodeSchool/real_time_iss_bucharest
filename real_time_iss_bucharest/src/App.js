695764aee7f5b2856b66bb95165aa2d9db0e4d50
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-11-08 15:32:24 +0200

    newsletter styling + pop-up IN WORK

--- real_time_iss_bucharest/src/App.js
+++ real_time_iss_bucharest/src/App.js
@@ -3,16 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 import Newsletter from './components/Newsletter'
+import Modal from './components/Modal'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { tsConstructorType } from '@babel/types';
 
 
-function App() {
-  return (
-    <div className="App">
-      <Newsletter />
-    </div>
-  );
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <Newsletter />
+//       <Modal show={false}/>
+//     </div>
+//   );
+// }
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      checkBoxThatOpensModal : false
+    }
+  }
+
+  checkBoxThatOpensModalFunction = (dataFromCheckbox) => {
+    this.setState({checkBoxThatOpensModal: dataFromCheckbox})
+  }
+
+  dataFn(dataFromChild) {
+    console.log('called: ' + dataFromChild)
+    this.setState({
+      checkBoxThatOpensModal: dataFromChild
+    })
+  }
+
+  render(){
+    console.log(this.state.checkBoxThatOpensModal)
+    return (
+      <div className="App">
+        <Newsletter callBack={this.dataFn.bind(this)}/>
+        <Modal show={this.state.checkBoxThatOpensModal}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- real_time_iss_bucharest/src/components/Newsletter.js
+++ real_time_iss_bucharest/src/components/Newsletter.js
@@ -1,27 +1,34 @@
 import React, { Component } from 'react';
-import './newsletter.css'
+import '../App.css'
 
-import { Container, Form, Row, Col, Button, Image } from 'react-bootstrap';
+import CustomBtn from './Button'
+
+import { Container, Form, Row, Col, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 class Newsletter extends Component {
-    constructor() {
-        super()
-    }
 
-    render() {
-        return (
-            <Container className="bg-dark text-light form-container">
-                <Row>
-                    <Col xs={6} md={4}>
-                        <Image src="holder.js/171x180" rounded />
-                    </Col>
+handleCheck() {
+    console.log('checked')
+    this.props.callBack(true)
+}
 
-            <Col xs={6} md={8}>
+render() {
+    return (
+    <Container className="bg-dark text-light form-container" fluid>
+        <Row className='align-items-center rowThatWorks'>
+            <Col xs={6} md={{ span: 6, offset:0 }}>
+                <Image className='form-img img-fluid' src={require('../img/0701890_orig.jpg')} />
+            </Col>
+
+            <Col xs={6} md={{ span: 5, offset: 0 }}>
+                <h1 className="annoying-title display-5">Some title</h1>
                 <Form className="actualForm">
                     <Form.Group as={Row} controlId="formHorizontalFullName">
                         <Form.Label column sm={2}>
                             Full Name
-    </Form.Label>
+                    </Form.Label>
                         <Col sm={10}>
                             <Form.Control type="text" placeholder="Full name" />
                         </Col>
@@ -29,32 +36,30 @@ class Newsletter extends Component {
                     <Form.Group as={Row} controlId="formHorizontalEmail">
                         <Form.Label column sm={2}>
                             Email
-    </Form.Label>
+                        </Form.Label>
                         <Col sm={10}>
                             <Form.Control type="email" placeholder="Email" />
                         </Col>
                     </Form.Group>
 
-
                     <Form.Group as={Row} controlId="formHorizontalCheck">
-                        <Col sm={{ span: 5, offset: 1}}>
+                        <Col sm={{ span: 4, offset: 2}}>
                             <Form.Check label="Remember me" />
                         </Col>
-                        <Col sm={{ span: 5, offset: 1}}>
-                            <Form.Check label="I want to win a trip to ISS" />
+                        <Col sm={{ span: 4, offset: 2}}>
+                            <Form.Check label="I want to win a trip to ISS" onClick={this.handleCheck.bind(this)} />
                         </Col>
                     </Form.Group>
-
-                    {/* <Form.Group as={Row}>
-                        <Col sm={{ span: 10, offset: 2 }}>
-                            <Button type="submit" variant="light">Sign in</Button>
-                        </Col>
-                    </Form.Group> */}
+                    <CustomBtn
+                    text='Sign up'
+                    bootstrapBtnTheme='light'
+                    type='submit'
+                    />
                 </Form>
-                </Col>
+            </Col>
 
-                </Row>
-            </Container>
+        </Row>
+    </Container>
         )
     }
 }
