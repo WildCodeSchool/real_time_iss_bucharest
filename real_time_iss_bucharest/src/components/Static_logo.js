50c140c4842371411efb7351d5b6eba90afe2bf2
Maria <fmariamarinescu@gmail.com>
2019-11-08 13:15:22 +0200

    creates static-logo component

---
+++ real_time_iss_bucharest/src/components/Static_logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StaticLogo = ({image}) => (
+    <img src={image} alt="static-logo" style={{position: relative, top: "1%", left: "20px", width: "100px", height: "100px"}}/>
+)
+
+
+
+export default StaticLogo
