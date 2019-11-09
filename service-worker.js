08630c13f39e79aedfb81ea89364dad0503b9f99
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-11-09 16:38:05 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/real_time_bucharest/precache-manifest.00eda414931cde686406fa744dd8d0e5.js"
+  "/real_time_iss_bucharest/precache-manifest.3a817e5ce11511a930150e4d53cd533e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/real_time_bucharest/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/real_time_iss_bucharest/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
