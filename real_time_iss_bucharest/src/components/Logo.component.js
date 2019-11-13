c492129626e2311aaccb6a26a6c86943cd4e1a1f
Maria <fmariamarinescu@gmail.com>
2019-11-13 12:33:27 +0200

    logo work done

---
+++ real_time_iss_bucharest/src/components/Logo.component.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Logo extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+       <div className="wrap">
+           <div clasName="side n-1"></div>
+           </div>
+        )
+    }
+}
