140f248614e42ac83fee423c5b94edc7045d62c9
Olivier Ggr <grandgerard.olivier@gmail.com>
2019-11-14 15:46:23 +0200

    map tweaking: start position with states

--- real_time_iss_bucharest/src/App.js
+++ real_time_iss_bucharest/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 
 import './App.css'
 import GMap from './components/Map'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- real_time_iss_bucharest/src/components/Map.js
+++ real_time_iss_bucharest/src/components/Map.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import axios from 'axios'
-import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css'
+
 
 const apiKey = 'AIzaSyBuqivjlUHx_1CzHXli6ft9xLWrI-dOGwo'
 const style = {
@@ -15,58 +17,63 @@ class GMap extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            longitude : '26.1025',
-            latitude : '44.4268'
+            longitude : '',
+            latitude : ''
         }
     }
 
     getCoordinates = () => {
-        const longitude = this.state.longitude
-        const latitude = this.state.latitude
-        axios.get('http://api.open-notify.org/iss-now.json')
-        .then(response => {
-            this.setState({
-                longitude : response.data.iss_position.longitude,
-                latitude : response.data.iss_position.latitude
+        this.timer = setInterval(() => {
+            console.log('new coordinates at ' + new Date().toLocaleTimeString())
+            axios.get('http://api.open-notify.org/iss-now.json')
+            .then(response => {
+                this.setState({
+                    longitude : response.data.iss_position.longitude,
+                    latitude : response.data.iss_position.latitude
+                })
             })
-        })
+        }, 2000);
     }
+
+    // getInitialCenter() {
+    //     let initialLong = this.state.longitude
+    //     let initialLat = this.state.latitude
+    // }
+
+    // componentDidMount() {
+
+    // }
 
     render() {
         return (
-            <div className="map_container map">
-                <h1>This is our beautiful map</h1>
-                <div className="btn-container">
-                <button className="test" onClick={this.getCoordinates}>
-                        click me
-                </button>
-                    <p>long: {this.state.longitude}</p>
-                    <p>lat: {this.state.latitude}</p>
+            <div className="map_container map ">
+                    <h1 className="display-4">ISS real-time position</h1>
+                    <div className="btn-container">
+                        <button className="test" onClick={this.getCoordinates}>
+                            click me
+                        </button>
+                        <div className="coordinates-info">
+                            <p>long: {this.state.longitude}</p>
+                            <p>lat: {this.state.latitude}</p>
+                        </div>
                     </div>
-                <div className="justTheMap">
-                <Map google={this.props.google} zoom={2}  
-                style={style} initialCenter={{
-                    lat: 44.4268,
-                    lng: 26.1025
+
+                <div className="justTheMap p-3 mb-2 bg-dark text-white">
+                <Map google={this.props.google} zoom={5}  
+                style={style} center={{
+                    lat: this.state.latitude,
+                    lng: this.state.longitude
                     }}>
 
- 
-                          <Marker
-    title={'Bucharest'}
-    name={'Bucharest'}
-    position={{lat: this.state.latitude, lng: this.state.longitude}}
-    // icon={{
-    //     url: "/path/to/custom_icon.png",
-    //     anchor: new google.maps.Point(32,32),
-    //     scaledSize: new google.maps.Size(64,64)
-    //   }}
-       />
+                <Marker
+                    title={'Bucharest'}
+                    name={'Bucharest'}
+                    position={{lat: this.state.latitude, lng: this.state.longitude}}/>
 
-                    {/* <InfoWindow onClose={this.onInfoWindowClose}>
-
-                    </InfoWindow> */}
                 </Map>
                 </div>
+                    <hr/>
+
 
 
             </div>
